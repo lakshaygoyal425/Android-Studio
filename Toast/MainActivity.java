492aa9dde026e18c6509ab7e6bc74137340e6579
Lakshay Goyal <47741067+lakshaygoyal425@users.noreply.github.com>
2020-09-21 09:45:49 +0530

    Add files via upload

---
+++ Toast/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayToast(View view){
+        Toast.makeText(MainActivity.this, "Welcome to the App",Toast.LENGTH_SHORT).show();
+
+
+    }
+}
